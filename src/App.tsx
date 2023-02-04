d25a02134857000f70d999890597fe4ee657d893
Karbonara <37299335+karbonara@users.noreply.github.com>
2023-02-04 20:39:59 +0300

    Добавляет тему в проект

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import classes from './style.module.scss';
 import Home from './pages/Home';
 import { AboutAsync } from './pages/About/About.async';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className={classes.text}>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       App
       <Link to={'home'}>home</Link>
       <Link to={'about'}>about</Link>
@@ -18,3 +21,7 @@ const App = () => {
 };
 
 export default App;
+function setTheme(arg0: any) {
+  throw new Error('Function not implemented.');
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Suspense } from 'react';
+import ThemeProvider from './theme/ThemeProvider';
 
 render(
   <Suspense fallback={<div>Loading...</div>}>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Suspense>,
   document.getElementById('root')
